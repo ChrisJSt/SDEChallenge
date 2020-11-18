5f945010a41fdb96959cc2db593b39f102239827
Chris <chrisjstelmach@gmail.com>
2020-11-17 21:37:37 -0500

    Fixing indent

--- src/main/java/chris/App.java
+++ src/main/java/chris/App.java
@@ -4,7 +4,7 @@ public class App {
 	
 	public static void main(String[] args) throws Exception {
 		
-		int[] arrNumElementsToAvg = {2,3,0};
+	int[] arrNumElementsToAvg = {2,3,0};
         Double[] sampleElements = {1.0, 5.0, 3.0, 4.0, 11.0};
         for (int numElementsToAvg : arrNumElementsToAvg) {
         	IMovingAverage simpleMovingAverage = new SimpleMovingAverage(numElementsToAvg);
